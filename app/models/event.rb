3f59bc114ee5343ed83c4b26867a54d43563eb51
Christian Schulmeister <cschulme@uncc.edu>
2018-03-28 21:20:07 +0000

    Added validation for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,12 @@
 class Event < ApplicationRecord
+    validates :title, :startTime, :endTime, presence: true
+    validate :checkStartAndEndTime
+    
+    def checkStartAndEndTime
+        if self.startTime.present? && self.endTime.present? && self.startTime < self.endTime 
+            return true
+        else
+            self.errors[:base] << 'Starting time should be before the ending time!'
+        end
+    end
 end
