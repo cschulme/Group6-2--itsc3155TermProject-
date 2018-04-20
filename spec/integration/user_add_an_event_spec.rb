ef5053f7a1dc4751a183583ef6ffda6a26647b3d
Christian Schulmeister <cschulme@uncc.edu>
2018-04-20 18:03:30 +0000

    add first user story with scenarios

--- spec/integration/user_add_an_event_spec.rb
+++ spec/integration/user_add_an_event_spec.rb
@@ -7,27 +7,40 @@ feature "User adds an article" do
         expect(page).to have_content("Your Events")
         click_link "New Event"
         expect(page).to have_content("Add New Event")
-=begin
-        expect(page).to have_field("Title:")
-	    expect(page).to have_field("Location:")
-        expect(page).to have_field("Information:")
-        expect(page).to have_field("Date:")
-        expect(page).to have_field("Starting Time:")
-        expect(page).to have_field("Ending Time:")
-        expect(page).to have_field("Tag:")
-=end
+        expect(page).to have_field("event[title]")
+	    expect(page).to have_field("event[location]")
+        expect(page).to have_field("event[eventInformation]")
+        expect(page).to have_select("event[eventDate(2i)]")
+        expect(page).to have_select("event[eventDate(3i)]")
+        expect(page).to have_select("event[eventDate(1i)]")
+        expect(page).to have_select("event[startTime(4i)]")
+        expect(page).to have_select("event[startTime(5i)]")
+        expect(page).to have_select("event[endTime(4i)]")
+        expect(page).to have_select("event[endTime(5i)]")
+        expect(page).to have_select("event[tag_id]")
     end
    
-    scenario "User successfully creates a new event" do
+    scenario "User successfully creates a new event with associated tag" do
+        # Create the tag.
+        visit new_tag_path
+        fill_in "tag[tagName]", with:  "Test"
+    	fill_in "tag[description]", with: "Capybara tag"
+    	click_button "Create Tag"
+    	
+    	# Create the event associated to the tag.
 	    visit new_event_path
     	expect(page).to have_content("Add New Event")
-	    fill_in "Title:", with:  "New Capybara Event"
-    	fill_in "Location:", with: "Charlotte"
-    	fill_in "Information:", with: "This is a new Capybara event"
-    	fill_in "Date:", with: "04-01-2018"
-    	fill_in "Starting Time:", with: "1pm"
-    	fill_in "Ending Time:", with: "2pm"
-    	fill_in "Tag:", with: "UNCC"
+	    fill_in "event[title]", with: "New Capybara Event"
+    	fill_in "event[location]", with: "Charlotte"
+    	fill_in "event[eventInformation]", with: "This is a new Capybara event"
+    	select "April", :from => "event[eventDate(2i)]"
+    	select "1", :from => "event[eventDate(3i)]"
+    	select "2018", :from => "event[eventDate(1i)]"
+    	select "01 PM", :from => "event[startTime(4i)]"
+    	select "00", :from => "event[startTime(5i)]"
+    	select "02 PM", :from => "event[endTime(4i)]"
+    	select "00", :from => "event[endTime(5i)]"
+    	select "Test", :from => "event[tag_id]"
     	click_button "Create Event"
     	expect(page).to have_content("New Capybara Event")
     end
@@ -40,9 +53,33 @@ feature "User deletes an event" do
         expect(page).to have_content ("Your Events")
     end
     
-    scenario "User successfully deletes an event" do
+    scenario "User successfully deletes their event" do
+        # Create the tag.
+        visit new_tag_path
+        fill_in "tag[tagName]", with:  "Test"
+    	fill_in "tag[description]", with: "Capybara tag"
+    	click_button "Create Tag"
+    	
+    	# Create the event associated to that tag.
+	    visit new_event_path
+    	expect(page).to have_content("Add New Event")
+	    fill_in "event[title]", with: "New Capybara Event"
+    	fill_in "event[location]", with: "Charlotte"
+    	fill_in "event[eventInformation]", with: "This is a new Capybara event"
+    	select "April", :from => "event[eventDate(2i)]"
+    	select "1", :from => "event[eventDate(3i)]"
+    	select "2018", :from => "event[eventDate(1i)]"
+    	select "01 PM", :from => "event[startTime(4i)]"
+    	select "00", :from => "event[startTime(5i)]"
+    	select "02 PM", :from => "event[endTime(4i)]"
+    	select "00", :from => "event[endTime(5i)]"
+    	select "Test", :from => "event[tag_id]"
+    	click_button "Create Event"
+    	expect(page).to have_content("New Capybara Event")
+    	
+    	# Delete that event.
         visit events_path
-        click_button "Delete Event"
+        click_link "Delete Event"
         expect(page).to have_content ("Your Events")
     end
 end
@@ -52,17 +89,17 @@ feature "User creates a new tag" do
     scenario "User successfully navigates to the Add New Tag page from the Your Tags page" do
         visit tags_path
         expect(page).to have_content("Your Tags")
-        click_button "New Tag"
+        click_link "New Tag"
         expect(page).to have_content("Add New Tag")
-        expect(page).to have_field("Tag Name:")
-	    expect(page).to have_field("Description:")
+        expect(page).to have_field("tag[tagName]")
+	    expect(page).to have_field("tag[description]")
     end
    
     scenario "User successfully creates a new tag" do
-	    visit new_tags_path
+	    visit new_tag_path
     	expect(page).to have_content("Add New Tag")
-	    fill_in "Title:", with:  "Test"
-    	fill_in "Description:", with: "Capybara tag"
+	    fill_in "tag[tagName]", with:  "Test"
+    	fill_in "tag[description]", with: "Capybara tag"
     	click_button "Create Tag"
     	expect(page).to have_content("Test")
     end
